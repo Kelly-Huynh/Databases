473a5c2512c9137e574337d27438a7b73ad4aea4
Kelly-Huynh <96kellyhuynh@gmail.com>
2026-04-27 20:44:36 +0100

    Initial commit: SQL bites

---
+++ SQL_bites_notes.sql
@@ -0,0 +1,24 @@
+-- SELECT release_year FROM albums
+--  WHERE title = 'Bossanova';
+
+-- UPDATE albums
+--  SET release_year = '2000', title = 'Whatever'
+--  WHERE id = '1';
+
+-- DELETE FROM album WHERE id = '1';
+
+-- UPDATE albums SET release_year = '1972' WHERE id = '3';
+
+-- INSERT INTO artists
+--     ( name, genre )
+--     VALUES( 'Massive Attack', 'Alternative' );
+
+-- INSERT INTO albums
+--     (title, release_year)
+--     VALUES( 'Mezzanine', '1998');
+
+-- UPDATE albums SET artist_id = '5' WHERE title = Mezzanine; <= forgot ''
+
+-- UPDATE albums SET artist_id = '5' WHERE title = 'Mezzanine'; <= correction on second attempt
+
+-- UPDATE albums SET artist_id = '5' WHERE id = '13'; <= alternatively
